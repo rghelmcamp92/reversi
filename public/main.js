96c2e4d7c0632c413fe7b399682f71294260077f
Randall Helmcamp <rghelmcamp@gmail.com>
2020-05-14 16:54:16 -0500

    no message

---
+++ public/main.js
@@ -0,0 +1,22 @@
+/* Functions for general use */
+/* Returns the value associated with 'whichparam' on the URL */
+
+function getURLParameters(whichParam){
+
+    var pageURL = window.location.search.substring(1);
+    var pageURLVariables = pageURL.split('&');
+    for (var i = 0; i < pageURLVariables.length; i++){
+
+        var parameterName = pageURLVariables[i].split('=');
+        if(parameterName[0] == whichParam){
+            return parameterName[1];
+        }
+    }
+}
+
+var username = getURLParameters('username');
+if ('undefined' == typeof username || !username){
+    username = 'Anonymous' + Math.random();
+}
+
+$('#messages').append('<h4>' + username +'</h4>');
